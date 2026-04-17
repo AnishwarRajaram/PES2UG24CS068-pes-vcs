c19536a1cbe34e5780c88b762abfc9646f4e34ff
AnishwarRajaram <rajaramanishwar@gmail.com>
2026-04-17 22:57:41 +0530

    header include fix

--- object.c
+++ object.c
@@ -13,6 +13,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <openssl/evp.h>
